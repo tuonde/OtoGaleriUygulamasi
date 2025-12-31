995f18c8004acdee89a9c9c4d9772b0ada43996a
Tunahan Onursal Demiral <tunahandemiral1963@gmail.com>
2025-12-31 19:04:36 +0300

    Program.cs güncellendi - başlangıç formu değiştirildi

---
+++ OtoGaleriUygulamasi/FormAnaEkran.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace OtoGaleriUygulamasi
+{
+    partial class FormAnaEkran
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSatildi = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnYeniIlan = new System.Windows.Forms.Button();
+            this.btnDuzenle = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ilanlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.raporlarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panelDGW = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.panelDGW.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(142)))), ((int)(((byte)(150)))));
+            this.panel1.Controls.Add(this.btnSatildi);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnSil);
+            this.panel1.Controls.Add(this.btnYeniIlan);
+            this.panel1.Controls.Add(this.btnDuzenle);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(984, 89);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSatildi
+            // 
+            this.btnSatildi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSatildi.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSatildi.ForeColor = System.Drawing.Color.White;
+            this.btnSatildi.Location = new System.Drawing.Point(321, 3);
+            this.btnSatildi.Name = "btnSatildi";
+            this.btnSatildi.Size = new System.Drawing.Size(100, 40);
+            this.btnSatildi.TabIndex = 4;
+            this.btnSatildi.Text = "Satıldı";
+            this.btnSatildi.UseVisualStyleBackColor = true;
+            this.btnSatildi.Click += new System.EventHandler(this.btnSatildi_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(67, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "OTO GALERİ YÖNETİM SİSTEMİ";
+            // 
+            // btnSil
+            // 
+            this.btnSil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSil.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.ForeColor = System.Drawing.Color.White;
+            this.btnSil.Location = new System.Drawing.Point(215, 3);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(100, 40);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click_1);
+            // 
+            // btnYeniIlan
+            // 
+            this.btnYeniIlan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(114)))), ((int)(((byte)(142)))), ((int)(((byte)(150)))));
+            this.btnYeniIlan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnYeniIlan.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYeniIlan.ForeColor = System.Drawing.Color.White;
+            this.btnYeniIlan.Location = new System.Drawing.Point(3, 3);
+            this.btnYeniIlan.Name = "btnYeniIlan";
+            this.btnYeniIlan.Size = new System.Drawing.Size(100, 40);
+            this.btnYeniIlan.TabIndex = 1;
+            this.btnYeniIlan.Text = "Yeni İlan";
+            this.btnYeniIlan.UseVisualStyleBackColor = false;
+            this.btnYeniIlan.Click += new System.EventHandler(this.btnYeniIlan_Click_1);
+            // 
+            // btnDuzenle
+            // 
+            this.btnDuzenle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDuzenle.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDuzenle.ForeColor = System.Drawing.Color.White;
+            this.btnDuzenle.Location = new System.Drawing.Point(109, 3);
+            this.btnDuzenle.Name = "btnDuzenle";
+            this.btnDuzenle.Size = new System.Drawing.Size(100, 40);
+            this.btnDuzenle.TabIndex = 2;
+            this.btnDuzenle.Text = "Düzenle";
+            this.btnDuzenle.UseVisualStyleBackColor = true;
+            this.btnDuzenle.Click += new System.EventHandler(this.btnDuzenle_Click_1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem,
+            this.ilanlarToolStripMenuItem,
+            this.raporlarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(984, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // ilanlarToolStripMenuItem
+            // 
+            this.ilanlarToolStripMenuItem.Name = "ilanlarToolStripMenuItem";
+            this.ilanlarToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.ilanlarToolStripMenuItem.Text = "İlanlar";
+            // 
+            // raporlarToolStripMenuItem
+            // 
+            this.raporlarToolStripMenuItem.Name = "raporlarToolStripMenuItem";
+            this.raporlarToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.raporlarToolStripMenuItem.Text = "Raporlar";
+            // 
+            // panelDGW
+            // 
+            this.panelDGW.Controls.Add(this.dataGridView1);
+            this.panelDGW.Location = new System.Drawing.Point(0, 136);
+            this.panelDGW.Name = "panelDGW";
+            this.panelDGW.Size = new System.Drawing.Size(984, 361);
+            this.panelDGW.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(984, 361);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(0, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "İlan Listesi";
+            // 
+            // FormAnaEkran
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(236)))), ((int)(((byte)(237)))));
+            this.ClientSize = new System.Drawing.Size(984, 497);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panelDGW);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "FormAnaEkran";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OTO GALERİ YÖNETİM SİSTEMİ";
+            this.Load += new System.EventHandler(this.FormAnaEkran_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panelDGW.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSatildi;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnDuzenle;
+        private System.Windows.Forms.Button btnYeniIlan;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ilanlarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem raporlarToolStripMenuItem;
+        private System.Windows.Forms.Panel panelDGW;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ OtoGaleriUygulamasi/FormAnaEkran.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using OtoGaleriUygulamasi.DAL;
+using OtoGaleriUygulamasi.Helpers;
+
+namespace OtoGaleriUygulamasi
+{
+    public partial class FormAnaEkran : Form
+    {
+        public FormAnaEkran()
+        {
+            InitializeComponent();
+        }
+
+        private void FormAnaEkran_Load(object sender, EventArgs e)
+        {
+            // Veritabanı bağlantısını test et
+            if (!DatabaseHelper.TestConnection())
+            {
+                MessageBox.Show("Veritabanına bağlanılamadı! Lütfen bağlantı ayarlarını kontrol edin.",
+                    "Bağlantı Hatası", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            IlanlariYukle();
+            DataGridViewAyarla();
+        }
+
+        private void IlanlariYukle()
+        {
+            DataTable dt = IlanDAL.TumIlanlariGetir();
+            dataGridView1.DataSource = dt;
+        }
+
+        private void DataGridViewAyarla()
+        {
+            // DataGridView görünüm ayarları
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+
+            // Kolon başlıklarını Türkçeleştir
+            if (dataGridView1.Columns.Count > 0)
+            {
+                dataGridView1.Columns["IlanID"].HeaderText = "İlan No";
+                dataGridView1.Columns["MarkaAdi"].HeaderText = "Marka";
+                dataGridView1.Columns["ModelAdi"].HeaderText = "Model";
+                dataGridView1.Columns["Fiyat"].HeaderText = "Fiyat";
+                dataGridView1.Columns["Fiyat"].DefaultCellStyle.Format = "C2"; // Para formatı
+                dataGridView1.Columns["Yil"].HeaderText = "Yıl";
+                dataGridView1.Columns["YakitTipiAdi"].HeaderText = "Yakıt";
+                dataGridView1.Columns["VitesTipiAdi"].HeaderText = "Vites";
+                dataGridView1.Columns["Kilometre"].HeaderText = "KM";
+                dataGridView1.Columns["Kilometre"].DefaultCellStyle.Format = "N0"; // Binlik ayraç
+                dataGridView1.Columns["KasaTipiAdi"].HeaderText = "Kasa";
+                dataGridView1.Columns["RenkAdi"].HeaderText = "Renk";
+                dataGridView1.Columns["AgirHasar"].HeaderText = "Ağır Hasar Kaydı";
+                dataGridView1.Columns["Durum"].HeaderText = "Durum";
+                dataGridView1.Columns["IlanTarihi"].HeaderText = "İlan Tarihi";
+                dataGridView1.Columns["IlanTarihi"].DefaultCellStyle.Format = "dd.MM.yyyy";
+            }
+        }
+
+        
+        private void btnYeniIlan_Click(object sender, EventArgs e)
+        {
+            // İleride FormIlanEkle açılacak
+            MessageBox.Show("İlan ekleme formu yakında eklenecek!");
+        }
+
+        // Düzenle butonu
+        private void btnDuzenle_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Lütfen düzenlenecek ilanı seçiniz.", "Uyarı",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int ilanID = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["IlanID"].Value);
+            MessageBox.Show("İlan düzenleme formu yakında eklenecek! İlan ID: " + ilanID);
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Lütfen silinecek ilanı seçiniz.", "Uyarı",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int ilanID = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["IlanID"].Value);
+
+            DialogResult result = MessageBox.Show("İlanı silmek istediğinize emin misiniz?",
+                "Silme Onayı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                if (IlanDAL.IlanSil(ilanID))
+                {
+                    MessageBox.Show("İlan başarıyla silindi.", "Başarılı",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    IlanlariYukle();
+                }
+            }
+        }
+
+        private void btnSatildi_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Lütfen satıldı olarak işaretlenecek ilanı seçiniz.", "Uyarı",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int ilanID = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["IlanID"].Value);
+
+            DialogResult result = MessageBox.Show("İlan satıldı olarak işaretlensin mi?",
+                "Onay", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                if (IlanDAL.IlanSatildi(ilanID))
+                {
+                    MessageBox.Show("İlan satıldı olarak işaretlendi.", "Başarılı",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    IlanlariYukle();
+                }
+            }
+        }
+        
+        private void btnYeniIlan_Click_1(object sender, EventArgs e)
+        {
+            // İleride FormIlanEkle açılacak
+            MessageBox.Show("İlan ekleme formu yakında eklenecek!");
+        }
+
+        private void btnDuzenle_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Lütfen düzenlenecek ilanı seçiniz.", "Uyarı",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int ilanID = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["IlanID"].Value);
+            MessageBox.Show("İlan düzenleme formu yakında eklenecek! İlan ID: " + ilanID);
+        }
+
+        private void btnSil_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Lütfen silinecek ilanı seçiniz.", "Uyarı",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int ilanID = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["IlanID"].Value);
+
+            DialogResult result = MessageBox.Show("İlanı silmek istediğinize emin misiniz?",
+                "Silme Onayı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                if (IlanDAL.IlanSil(ilanID))
+                {
+                    MessageBox.Show("İlan başarıyla silindi.", "Başarılı",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    IlanlariYukle();
+                }
+            }
+        }
+
+        private void btnSatildi_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Lütfen satıldı olarak işaretlenecek ilanı seçiniz.", "Uyarı",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            int ilanID = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["IlanID"].Value);
+
+            DialogResult result = MessageBox.Show("İlan satıldı olarak işaretlensin mi?",
+                "Onay", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                if (IlanDAL.IlanSatildi(ilanID))
+                {
+                    MessageBox.Show("İlan satıldı olarak işaretlendi.", "Başarılı",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    IlanlariYukle();
+                }
+            }
+        }
+    }
+}
